583e6a3cf5f1f0a893d9eef3929f682b1f501fd5
Гулзат <gulzat@MacBook-Pro-Gulzat.local>
2026-05-08 14:53:13 +0600

    add startups api

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from src.database import Base, engine
 from src.models import User, Startup, Connection
 
 from src.routers.users import router as users_router
+from src.routers.startups import router as startups_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(startups_router)
 
 @app.get("/")
 def home():
