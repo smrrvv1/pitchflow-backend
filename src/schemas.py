a87871fbbf2d5457544eadb066774d70b617181b
smrrvv1 <samira.b@compasscollege.art>
2026-05-08 11:04:53 +0600

    add pydantic schemas

---
+++ src/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: str
+    password: str
+    role: str
+
+
+class StartupCreate(BaseModel):
+    title: str
+    one_liner: str
+    description: str
+    tags: str
+    stage: str
+    target_amount: str
+    owner_id: int
+
+
+class ConnectionCreate(BaseModel):
+    startup_id: int
+    investor_id: int
