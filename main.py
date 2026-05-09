0477b752a56b2ac4ecfd1f60110ac82e274c7fe4
smrrvv1 <samira.b@compasscollege.art>
2026-05-09 19:58:24 +0600

    fix problems

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from src.database import Base, engine
@@ -10,6 +11,13 @@ from src.routers.connections import router as connections_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(startups_router)
